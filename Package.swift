5ea5123c0f88b26ac2da6344cd9aa24f25bf2f85
Adir Burke on Laptop <adir@hanave.com>
2020-09-15 09:36:16 +1000

    Updating

--- Package.swift
+++ Package.swift
@@ -1,14 +1,19 @@
-// swift-tools-version:4.1
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
 	name: "PerfectNotifications",
+    platforms: [
+           .macOS(.v10_15),
+    ],
 	products: [
 		.library(name: "PerfectNotifications", targets: ["PerfectNotifications"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/PerfectlySoft/Perfect-Net.git", from: "4.0.0"),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", from: "3.0.0")
+		// .package(url: "https://github.com/PerfectlySoft/Perfect-Net.git", from: "4.0.0"),
+		.package(path: "~/Dev/Perfect/Perfect-Net"),
+		.package(path: "~/Dev/Perfect/Perfect-Thread"),
+		// .package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", from: "3.0.0")
 	],
 	targets: [
 		.target(name: "PerfectNotifications", dependencies: ["PerfectNet", "PerfectThread"])
